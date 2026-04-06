0a0a1baec9e215cf247c1bf7eca0769958a8627c
Paul Byford <paul-byford@github>
2026-04-06 12:50:25 +0100

    add cc sdk

--- sdk.ts
+++ sdk.ts
@@ -2,10 +2,13 @@
 // to "@anthropic-ai/claude-agent-sdk"
 import { query } from "@anthropic-ai/claude-agent-sdk";
 
-const prompt = "";
+const prompt = "Add a description to package.json.";
 
 for await (const message of query({
   prompt,
+  options: {
+    allowedTools: ["Edit"], // Allow the agent to use the file system tool
+  }
 })) {
   console.log(JSON.stringify(message, null, 2));
 }
